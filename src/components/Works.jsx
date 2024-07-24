7895a3ce1a2af8c1560c876b87eb1072033db822
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-24 16:27:46 +0300

    Fix mobile view

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,10 +16,11 @@ const ProjectCard = ({
   source_code_link,
   live_demo_link,
 }) => {
-  const showLiveDemo = live_demo_link !== undefined && live_demo_link !== '';
-  const showSource_code_link = source_code_link !== undefined && source_code_link !== '#';  
+  const showLiveDemo = live_demo_link && live_demo_link.trim() !== '';
+  const showSource_code_link = source_code_link && source_code_link.trim() !== '';
+
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)} className='relative'>
       <Tilt
         options={{
           max: 45,
@@ -28,7 +29,7 @@ const ProjectCard = ({
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
-        <div className=' w-full '>
+        <div className='relative'>
           <img
             src={image}
             alt='project_image'
@@ -37,21 +38,21 @@ const ProjectCard = ({
 
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             {showSource_code_link && (
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img
-                src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              >
+                <img
+                  src={github}
+                  alt='source code'
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
             )}
             {showLiveDemo && (
               <div
                 onClick={() => window.open(live_demo_link, "_blank")}
-                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer ml-2'
               >
                 <img
                   src={live_demo}
@@ -102,7 +103,7 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
+        <p className={`${styles.sectionSubText}`}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
@@ -111,9 +112,9 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          links to code repositories and live demos. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
