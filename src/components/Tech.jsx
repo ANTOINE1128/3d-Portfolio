76c18e2cc1530085e6195db9d62101c3328939c3
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-24 23:24:56 +0300

    Updatae mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useMediaQuery } from 'react-responsive';
 
 const Tech = () => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-4 sm:gap-10'>
       {technologies.map((technology) => (
         <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <BallCanvas icon={technology.icon} isStatic={!isDesktop} />
         </div>
       ))}
     </div>
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -3,34 +3,31 @@
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import { Decal, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import PropTypes from 'prop-types';
 import CanvasLoader from "../Loader";
+import { useMediaQuery } from 'react-responsive'; // Import react-responsive library
 
 const Ball = ({ imgUrl }) => {
   const [decal] = useTexture([imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+    <mesh castShadow receiveShadow scale={2.75}>
+      <icosahedronGeometry args={[1, 1]} />
+      <meshStandardMaterial
+        color='#fff8eb'
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+      />
+      <Decal
+        position={[0, 0, 1]}
+        rotation={[2 * Math.PI, 0, 6.25]}
+        scale={1}
+        map={decal}
+        flatShading
+      />
+    </mesh>
   );
 };
 
@@ -38,7 +35,9 @@ Ball.propTypes = {
   imgUrl: PropTypes.string,
 };
 
-const BallCanvas = ({ icon }) => {
+const BallCanvas = ({ icon, isStatic }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' }); // Mobile view
+
   return (
     <Canvas
       frameloop='demand'
@@ -50,7 +49,13 @@ const BallCanvas = ({ icon }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        {isMobile ? (
+          <Ball imgUrl={icon} />
+        ) : (
+          <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+            <Ball imgUrl={icon} />
+          </Float>
+        )}
       </Suspense>
 
       <Preload all />
@@ -60,6 +65,7 @@ const BallCanvas = ({ icon }) => {
 
 BallCanvas.propTypes = {
   icon: PropTypes.string,
+  isStatic: PropTypes.bool,
 };
 
 export default BallCanvas;
