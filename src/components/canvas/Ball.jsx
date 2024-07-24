fd88fcb404d699108cdb8cd6e3f917fac33c1ec5
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-24 23:28:28 +0300

    Fix bugs

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -3,31 +3,34 @@
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import PropTypes from 'prop-types';
 import CanvasLoader from "../Loader";
-import { useMediaQuery } from 'react-responsive'; // Import react-responsive library
 
 const Ball = ({ imgUrl }) => {
   const [decal] = useTexture([imgUrl]);
 
   return (
-    <mesh castShadow receiveShadow scale={2.75}>
-      <icosahedronGeometry args={[1, 1]} />
-      <meshStandardMaterial
-        color='#fff8eb'
-        polygonOffset
-        polygonOffsetFactor={-5}
-        flatShading
-      />
-      <Decal
-        position={[0, 0, 1]}
-        rotation={[2 * Math.PI, 0, 6.25]}
-        scale={1}
-        map={decal}
-        flatShading
-      />
-    </mesh>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
   );
 };
 
@@ -35,9 +38,7 @@ Ball.propTypes = {
   imgUrl: PropTypes.string,
 };
 
-const BallCanvas = ({ icon, isStatic }) => {
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' }); // Mobile view
-
+const BallCanvas = ({ icon }) => {
   return (
     <Canvas
       frameloop='demand'
@@ -49,13 +50,7 @@ const BallCanvas = ({ icon, isStatic }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        {isMobile ? (
-          <Ball imgUrl={icon} />
-        ) : (
-          <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-            <Ball imgUrl={icon} />
-          </Float>
-        )}
+        <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
@@ -65,7 +60,6 @@ const BallCanvas = ({ icon, isStatic }) => {
 
 BallCanvas.propTypes = {
   icon: PropTypes.string,
-  isStatic: PropTypes.bool,
 };
 
 export default BallCanvas;
