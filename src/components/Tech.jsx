1c4f8955663bbc75d59ed70ce012f14afb01bbd1
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-25 12:36:57 +0300

    Fix mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,7 +9,7 @@ const Tech = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
   // Display only the first 6 technologies on mobile
-  const displayedTechnologies = isMobile ? technologies.slice(0, 6) : technologies;
+  const displayedTechnologies = isMobile ? technologies.slice(0, 8) : technologies;
 
   return (
     <div className='flex flex-row flex-wrap justify-center gap-4 sm:gap-10'>
