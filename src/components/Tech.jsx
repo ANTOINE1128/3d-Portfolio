b3a2bc7eaf69e8e6045170352cb7102920239fd5
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-25 12:32:45 +0300

    Fix tech view for mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,10 +6,14 @@ import { useMediaQuery } from 'react-responsive';
 
 const Tech = () => {
   const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
+  // Display only the first 6 technologies on mobile
+  const displayedTechnologies = isMobile ? technologies.slice(0, 6) : technologies;
 
   return (
     <div className='flex flex-row flex-wrap justify-center gap-4 sm:gap-10'>
-      {technologies.map((technology) => (
+      {displayedTechnologies.map((technology) => (
         <div className='w-20 h-20 sm:w-28 sm:h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} isStatic={!isDesktop} />
         </div>
