25fd4775cbcaeff6f473a97adb322eb31cdee059
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-25 12:27:40 +0300

    Fix mobile view hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,16 +5,17 @@ import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
   const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
   return (
-    <section className={`relative w-full ${isDesktop ? 'h-screen' : 'h-[80vh]'}`}>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col ${isDesktop ? 'md:flex-row' : 'flex-col'} items-start gap-5`}>
-        <div className={`flex flex-col justify-center items-center mt-5 ${isDesktop ? '' : 'hidden'}`}>
+    <section className={`relative w-full h-screen ${isMobile ? 'h-[80vh]' : 'h-screen'}`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col md:flex-row items-start gap-5`}>
+        <div className={`flex flex-col justify-center items-center mt-5 ${isMobile ? 'hidden' : 'block'}`}>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 h-40 sm:h-80 violet-gradient hidden sm:block' />
         </div>
 
-        <div className={`text-center ${isDesktop ? '' : 'text-left'}`}>
+        <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I&apos;m <span className='text-[#915eff]'>Antoine</span>
           </h1>
@@ -27,23 +28,21 @@ const Hero = () => {
 
       {isDesktop && <ComputersCanvas />}
       
-      {isDesktop && (
-        <div className="absolute bottom-32 w-full flex justify-center items-center">
-          <a href="#about">
-            <div className='mt-2 w-[30px] h-[54px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-              <motion.div
-                animate={{ y: [0, 24, 0] }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: 'loop'
-                }}
-                className='w-3 h-3 rounded-full bg-secondary mb-1'
-              />
-            </div>
-          </a>
-        </div>
-      )}
+      <div className="absolute bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className='mt-2 w-[30px] h-[54px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
